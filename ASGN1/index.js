9b7f70626b704c3f31f5f173eb814a9fe9d89113
omidmehdi <orezaei@ucsc.edu>
2026-01-18 18:16:44 -0800

    index fix?

--- ASGN1/index.js
+++ ASGN1/index.js
@@ -186,7 +186,7 @@ function addActionsForHtmlUI() {
   document.getElementById('pointButton').onclick = function() { g_selectedType = POINT; };
   document.getElementById('triButton').onclick = function() { g_selectedType = TRIANGLE; };
   document.getElementById('circleButton').onclick = function() { g_selectedType = CIRCLE; };
-  document.getElementById('pictureButton').onclick = function() { renderPicture(); };
+  document.getElementById('pictureButton').onclick = function() { drawPicture(); };
 
   // Slider Events (Color)
   document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
